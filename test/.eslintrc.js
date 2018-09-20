77a055338cc814cf901d781f283c5e4b973eb3af
Nandi Wong <nandi0315@gmail.com>
2018-09-20 18:57:36 +0800

    add coverage and testing setup

---
+++ test/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "mocha": true
+  },
+  "plugins": [
+    "import", "mocha", "chai-friendly", "node"
+  ],
+};
---
+++ test/test_cmd.js
@@ -0,0 +1,20 @@
+
+describe('generator', () => {
+
+});
+
+// function setupTestEnvironment(name) {
+//   const ctx = {};
+
+//   before('create environment', (done) => {
+//     ctx.dir = path.join(TEMP_DIR, name.replace(/[<>]/g, ''));
+//     mkdirp(ctx.dir, done);
+//   });
+
+//   after('cleanup environment', function (done) {
+//     this.timeout(30000);
+//     rimraf(ctx.dir, done);
+//   });
+
+//   return ctx;
+// }
